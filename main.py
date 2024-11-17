38b2657229dcb56b28591c2df153091e492aeb11
Rickard Lindberg <rickard@rickardlindberg.me>
2024-11-17 13:40:10 +0100

    Range for strings are only inside quotes.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 def selftest():
     """
     >>> compile_chain(["Json.file"], ' "hello" ')
-    ['String', Range(1, 8), 'hello']
+    ['String', Range(2, 7), 'hello']
 
     >>> compile_chain(["Json.file"], ' true ')
     ['True', Range(1, 5)]
@@ -23,7 +23,7 @@ def selftest():
     ['List', ['Number', Range(3, 4), 1], ['Number', Range(7, 8), 2], ['Number', Range(11, 12), 3]]
 
     >>> compile_chain(["Json.file"], ' { "hello" : 5 } ')
-    ['Dict', Range(1, 16), ['Entry', ['Key', Range(3, 10), 'hello'], ['Number', Range(13, 14), 5]]]
+    ['Dict', Range(1, 16), ['Entry', ['Key', Range(4, 9), 'hello'], ['Number', Range(13, 14), 5]]]
 
     >>> print(compile_chain(["Json.file", "Json.pretty"], ' { "hello" : [1, false, true, null], "there": "hello" } '), end="")
     {
