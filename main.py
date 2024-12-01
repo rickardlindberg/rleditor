0adfac71bf589ee322a8e6f4de68ef855feb1dbb
Rickard Lindberg <rickard@rickardlindberg.me>
2024-12-01 15:25:22 +0100

    Fail fast doctest option.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     if "--selftest" in sys.argv:
-        doctest.testmod(optionflags=doctest.REPORT_NDIFF)
+        doctest.testmod(optionflags=doctest.REPORT_NDIFF | doctest.FAIL_FAST)
         print("ok")
     else:
         GtkUi.create().run()
